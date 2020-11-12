4a5beb15643c5cf6be91c66d5a58b2d909056186
Mayank Dutta <33067129+mayankdutta@users.noreply.github.com>
2020-11-13 00:54:12 +0530

    Added Disjoint dataset
    competitve programming_3 pg no. 54

---
+++ dsu.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+class UnionFind {
+    std::vector <int> p;
+    std::vector <int> rank;
+    public:
+
+    UnionFind (int N) { 
+        rank.resize (N, 0); 
+        p.resize (N, 0); 
+
+        for (int i=0; i<N; i++) p[i] = i; 
+    }
+
+    int findSet (int i) { 
+        return (p[i] == i) ? i : (p[i] == findSet (p[i])); 
+    }
+
+    bool isSameSet (int i, int j) { 
+        return findSet(i) == findSet(j); 
+    }
+
+    void unionSet (int i, int j) { 
+        if (!isSameSet (i, j)) {
+            int x = findSet (i), y = findSet (j); 
+            if (rank [x] > rank[y]) p[y] = x; 
+            else { 
+                p[y] = y; 
+                if (rank [x] == rank[y]) rank [y] ++; 
+            }
+        }
+    }
+};
