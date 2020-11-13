28e250ed8b76950bb50a27b77f22b70ccb8f2c28
Mayank Dutta <33067129+mayankdutta@users.noreply.github.com>
2020-11-13 14:39:59 +0530

    Kadane algo

---
+++ kadane.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using ll=long long int; 
+
+ll kadane (const vector <ll> arr) { 
+    ll best = 0, mx = 0; 
+    for (ll i=0; i<arr.size(); i++) { 
+        mx = std::max ( arr[k], arr[k]+mx ); 
+        best = std::max ( best, mx ); 
+    }
+    return best; 
+}
