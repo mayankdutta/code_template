bf7119d77013f4c0007ba38fe1394748f4682ad3
Mayank Dutta <33067129+mayankdutta@users.noreply.github.com>
2020-11-21 01:28:47 +0530

    Added my general template

---
+++ template.cpp
@@ -0,0 +1,37 @@
+//THINK ALOUD !! 
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+using ll=long long int;
+using lld=long double;
+#define mp make_pair
+#define pb push_back
+#define accu accumulate 
+
+#define pll pair<ll,ll>
+#define pii pair<int,int> 
+#define pls pair<ll,string>
+#define psl pair<string,ll>
+#define pis pair<int,string>
+#define psi paid<string,int>
+
+#define rep( i, a, n ) for( ll i=a; i<n; i++)
+#define per( i, a, n ) for( ll i=n-1; i>=a; i--)
+#define repp( i, a, n, p ) for( ll i=a; i<n; i+=p)
+#define all(c) (c).begin(), (c).end()
+#define mod 1000000007
+
+template <typename t> inline void print ( const t & v ){
+    std::cout << "Printing the container: " << endl;
+    for ( const auto & i: v) {
+        std::cout << i << " ";
+    }
+    std::cout << endl;
+}
+
+int main () { 
+    std::cout << "Hello, Mayank how it's going " << endl;
+}
+
