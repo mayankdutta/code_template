99749d42e28fd7eb2f3e8ab6d703ae77bed2a8b0
Mayank Dutta <33067129+mayankdutta@users.noreply.github.com>
2020-12-11 19:43:08 +0530

    added a function

--- template.cpp
+++ template.cpp
@@ -56,6 +56,18 @@ unsigned long bin_to_dec ( string s ) {
     return decimal;
 }
 
+unsigned int next_highest_power_of2 (unsigned int v) { 
+    unsigned int v; // compute the next highest power of 2 of 32-bit v
+    v--;
+    v |= v >> 1;
+    v |= v >> 2;
+    v |= v >> 4;
+    v |= v >> 8;
+    v |= v >> 16;
+    v++;
+    return v; 
+}
+
 int main () { 
     cout << "Hello, World";
 }
