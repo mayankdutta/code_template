538ae9291353053f269c433c71c57f5a76025d91
Mayank Dutta <33067129+mayankdutta@users.noreply.github.com>
2020-12-20 12:05:24 +0530

    Added big integer library for factorial.

--- template.cpp
+++ template.cpp
@@ -1,6 +1,6 @@
 //THINK ALOUD !! 
 #include <bits/stdc++.h>
-#define what_is(x) cout << "value of " << #x << " :: " << x << '\n'  
+#define what_is(x) cout << "value of " << #x << " :: " << x << "\n" 
 
 using namespace std;
 using ll=long long int;
@@ -26,11 +26,49 @@ using lld=long double;
 #define all(c) (c).begin(), (c).end()
 #define mod 1000000007
 
+class Bignum {
+    public:
+        Bignum(int value) {
+            assert(value >= 0 && value <= 999999999);
+            parts.push_back(value);
+        }
+
+        Bignum& operator*=(int rhs) {
+            assert(rhs >= 0 && rhs <= 999999999);
+            uint32_t carry = 0;
+            for (size_t i = 0; i < parts.size(); i++) {
+                uint64_t product = (uint64_t)parts[i] * (uint64_t)rhs + carry;
+                parts[i] = (uint32_t)(product % 1000000000LL);
+                carry = (uint32_t)(product / 1000000000LL);
+            }
+            if (carry != 0) parts.push_back(carry);
+            return *this;
+        }
+
+        friend std::ostream & operator<<(std::ostream& stream, const Bignum& num);
+
+    private:
+        std::vector<uint32_t> parts;
+};
+
+inline std::ostream& operator<<(std::ostream& stream, const Bignum& num) {
+    char oldfill = stream.fill('0');
+    for (std::vector<uint32_t>::const_reverse_iterator it = num.parts.rbegin(); it != num.parts.rend(); it++)
+        stream << *it << std::setw(9);
+    stream.fill(oldfill);
+    stream.width(0);
+    return stream;
+}
+
+Bignum factorial(int n) {
+    Bignum fac = 1;
+    for (int i = 2; i <= n; i++) fac *= i;
+    return fac;
+}
+
 ll bin_pow (ll a, ll b) {
     if (b == 0) return 1;
-    
     long long res = bin_pow(a, b / 2);
-    
     if (b % 2) return res * res * a;
     else return res * res;
 }
@@ -53,7 +91,7 @@ unsigned long bin_to_dec (string s) {
 }
 
 unsigned int next_power_of2 (unsigned int v) { 
-//    unsigned int v; // compute the next highest power of 2 of 32-bit v
+    //    unsigned int v; // compute the next highest power of 2 of 32-bit v
     v--;
     v |= v >> 1;
     v |= v >> 2;
@@ -68,6 +106,7 @@ int digit (ll i) {
     return i > 0 ? (int) log10 ((double) i) + 1 : 1;
 }
 
-int main () { 
-    cout << "Hello, World";
+int main ( ) { 
+    fast; 
+    cout << "Hello, World\n"; 
 }
