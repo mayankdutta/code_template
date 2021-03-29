8e1770c8e98df923fe4b841eefb914767abe27ac
Mayank Dutta <33067129+mayankdutta@users.noreply.github.com>
2021-03-29 22:48:14 +0530

    setIO

--- template.cpp
+++ template.cpp
@@ -73,7 +73,7 @@ template <typename INT> std::map<INT, int> factorize(INT n) {
 void setIO(string name = "") { // name is nonempty for USACO file I/O
 	// ios_base::sync_with_stdio(0); cin.tie(0); // see Fast Input & Output
 	// alternatively, cin.tie(0)->sync_with_stdio(0);
-	if (sz(name)) {
+	if (!name.empty()) {
 		freopen((name+".in").c_str(), "r", stdin); // see Input & Output
 		freopen((name+".out").c_str(), "w", stdout);
 	}
