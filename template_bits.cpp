7b3611c7b427f6dce8340fa52bac680ac2b258d5
Mayank Sharma <info@Mayanks-MacBook-Air.local>
2021-05-20 21:21:51 +0530

    corrected bitwise

--- template_bits.cpp
+++ template_bits.cpp
@@ -1,14 +1,14 @@
-string dec_to_bin (int n) { 
-    string binary = std::bitset <8> ( n ).to_string ( ); 
-    return binary; 
+string dec_to_bin (int n) {
+    string binary = std::bitset <8> ( n ).to_string ( );
+    return binary;
 }
 
-unsigned long bin_to_dec (string s) { 
-    unsigned long decimal = std::bitset <8> ( s ).to_ulong ( ); 
+unsigned long bin_to_dec (string s) {
+    unsigned long decimal = std::bitset <8> ( s ).to_ulong ( );
     return decimal;
 }
 
-unsigned int next_power_of2 (unsigned int v) { 
+unsigned int next_power_of2 (unsigned int v) {
     //    unsigned int v; // compute the next highest power of 2 of 32-bit v
     v--;
     v |= v >> 1;
@@ -17,14 +17,14 @@ unsigned int next_power_of2 (unsigned int v) {
     v |= v >> 8;
     v |= v >> 16;
     v++;
-    return v; 
+    return v;
 }
 
-template<typename INT> bool oppositeSigns(INT x, INT y) { 
+template<typename INT> bool oppositeSigns(INT x, INT y) {
     return ((x ^ y) < 0);  // True if x and y have opposite signs.
     // source :: http://graphics.stanford.edu/~seander/bithacks.html
 }
 
-bool isPowerOfTwo(int n) { 
-    return (n & (n - 1) == 0); // note that 0 is considered incorrectly as power of 2
+bool isPowerOfTwo(int n) {
+    return (((n) & (n - 1)) == 0); // note that 0 is considered incorrectly as power of 2
 }
