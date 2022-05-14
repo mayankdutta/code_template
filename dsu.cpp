1a2d5690fe8243119079d01bd722e88b9a93e33e
mayank dutta <smayankdutt@gmail.com>
2022-05-14 15:37:13 +0530

    dsu updated

--- dsu.cpp
+++ dsu.cpp
@@ -1,76 +1,49 @@
+#include <algorithm>
 #include <iostream>
+#include <numeric>
 #include <vector>
 
 class UnionFind {
 public:
   std::vector<int> Parent;
   std::vector<int> Size;
+  int number_of_components;
+  int max_size_of_component;
+  int defaultSize;
 
+  UnionFind() : defaultSize(int(3e5) + 5) {}
   void init(int n) {
-    /* You may want to change the size. */
-    Size.resize(int(3e5) + 5, 1);
-    Parent.resize(int(3e5) + 5, 0);
+    Size.resize(std::max(n, defaultSize), 1);
+    Parent.resize(std::max(n, defaultSize), 0);
 
-    for (int i = 0; i < n; i++)
-      Parent[i] = i;
+    std::iota(Parent.begin(), Parent.end(), 0);
   }
 
-  void makeSet(int n) {
+  void add(int n) {
     Parent[n] = n;
     Size[n] = 1;
   }
 
-  int findSet(int i) {
-    return (Parent[i] == i) ? i : (Parent[i] = findSet(Parent[i]));
+  int find(int n) {
+    return (Parent[n] == n) ? n : (Parent[n] = find(Parent[n]));
   }
 
-  bool isSameSet(int i, int j) { return findSet(i) == findSet(j); }
+  int findSlow(int n) { return (n == Parent[n] ? n : findSlow(Parent[n])); }
 
-  void unionSet(int a, int b) {
-    a = findSet(a);
-    b = findSet(b);
+  bool same(int i, int j) { return find(i) == find(j); }
+
+  void merge(int a, int b) {
+    a = find(a); // fast one
+    b = find(b); // fast one
     if (a == b)
       return;
+
     if (Size[a] < Size[b])
       std::swap(a, b);
 
     Parent[b] = a;
     Size[a] += Size[b];
+    number_of_components--;
+    max_size_of_component = std::max(Size[a], max_size_of_component);
   }
 } dsu;
-
-class UnionFindSlow {
-public:
-  std::vector<int> Parent;
-  std::vector<int> Size;
-
-  void init(int n) {
-    /* You may want to change the size. */
-    Size.resize(int(3e5) + 5, 1);
-    Parent.resize(int(3e5) + 5, 0);
-
-    for (int i = 0; i < n; i++)
-      Parent[i] = i;
-  }
-
-  void makeSet(int n) {
-    Parent[n] = n;
-    Size[n] = 1;
-  }
-
-  int findSet(int v) { return (v == Parent[v] ? v : findSet(Parent[v])); }
-
-  bool isSameSet(int i, int j) { return findSet(i) == findSet(j); }
-
-  void unionSet(int a, int b) {
-    a = findSet(a);
-    b = findSet(b);
-    if (a == b)
-      return;
-    if (Size[a] < Size[b])
-      std::swap(a, b);
-
-    Parent[b] = a;
-    Size[a] += Size[b];
-  }
-} sdsu;
